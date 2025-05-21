2fd35b71e0cd7b447dd15ea91c2d1b8d541e6715
dritamahmudi <60691190+dritamahmudi@users.noreply.github.com>
2025-05-21 11:08:43 +0200

    Update contactform.js

--- js/contactform.js
+++ js/contactform.js
@@ -44,6 +44,9 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
                 if (result.success) {
                     successDiv.textContent = "Nachricht erfolgreich gesendet!";
                     form.reset();
+                    name = "";
+                    email = "";
+                    text = "";
                 } else {
                     errorDiv.textContent = "Fehler Unbekannter Fehler"};
                 }
