59647c906f23c33bd79418c2afe5eac2c5bedb3a
dritamahmudi <60691190+dritamahmudi@users.noreply.github.com>
2025-05-27 16:17:30 +0200

    Update aos-init.js

--- js/aos-init.js
+++ js/aos-init.js
@@ -3,16 +3,21 @@ AOS.init({
   once: false // Animationen werden jedes Mal ausgelöst, wenn das Element gescrollt wird
 });
 
+
 // Wenn der Button sichtbar wird, Animation starten:// Prüft, ob das Element die Klasse "aos-zoom-btn" hat
 document.addEventListener('aos:in', function(event) {
-  if (event.detail.id === 'aos-zoom-btn' || event.detail.classList.contains('aos-zoom-btn')) {
+  console.log('AOS IN ausgelöst für:', event.detail);
+  
+  if (event.detail.classList.contains('aos-zoom-btn')) {
     event.detail.classList.add('zoom-loop');
+     console.log('zoom-loop hinzugefügt:', event.detail);
   }
 });
-
 // Wenn der Button nicht mehr sichtbar ist, Animation entfernen:
 document.addEventListener('aos:out', function(event) {
-  if (event.detail.id === 'aos-zoom-btn' || event.detail.classList.contains('aos-zoom-btn')) {
+    console.log('AOS OUT ausgelöst für:', event.detail);
+  if (event.detail.classList.contains('aos-zoom-btn')) {
     event.detail.classList.remove('zoom-loop');
+    console.log('zoom-loop entfernt:', event.detail);
   }
 });
