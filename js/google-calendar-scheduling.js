e99aeeb25f6d336abe9c6ad0e4ea1c342b392647
dritamahmudi <60691190+dritamahmudi@users.noreply.github.com>
2025-05-27 09:06:56 +0200

    Create google-calendar-scheduling.js

---
+++ js/google-calendar-scheduling.js
@@ -0,0 +1,11 @@
+  (function() {
+               var target = document.currentScript;
+                window.addEventListener('load', function() {
+                calendar.schedulingButton.load({
+                url: 'https://calendar.google.com/calendar/appointments/AcZssZ072-TBkoHWLrXcmxJsKPS3RWgHX2ey0h7Fu4E=?gv=true',
+                color: '#ff4ff3',
+                label: "Book an Appointment!",
+                target,
+                      });
+                   });
+              })();
