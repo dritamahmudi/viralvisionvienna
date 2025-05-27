48815f6eadb9d03066df5f7484306c37a2392e0f
dritamahmudi <60691190+dritamahmudi@users.noreply.github.com>
2025-05-27 09:18:15 +0200

    Create swiper-init.js

---
+++ js/swiper-init.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
+});
